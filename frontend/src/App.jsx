06feea97d1b155e465bef7da8e2dc6da7bfd46f0
Shivam gupta <sguupta1214@gmail.com>
2024-05-29 00:54:38 +0530

    default pgae update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
    <>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<Signup />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
             <Route path="/dashboard" element={<Dashboard />} />
